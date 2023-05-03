9482429f3465ca9ab9e2e183d123146daafe2a56
lazychino <lazy.chino@gmail.com>
2023-05-03 14:38:10 -0400

    last state we ended the class on 4/28

--- counter.js
+++ counter.js
@@ -1,1 +1,8 @@
 // TODO: Add counter code here.
+
+const countSpan = document.getElementById('count');
+const incrementButton = document.querySelector('#increment');
+const decrementButton = document.querySelector('#decrement');
+console.log(countSpan);
+console.log(incrementButton);
+console.log(decrementButton);
